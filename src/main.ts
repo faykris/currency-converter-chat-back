41cf41c98f64c7b2ff66e6a9566432eba5ce4675
faykris <faykris28@gmail.com>
2024-07-22 12:14:51 -0500

    restore commit from other project

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
+  
   await app.listen(3002);
 }
 bootstrap();
